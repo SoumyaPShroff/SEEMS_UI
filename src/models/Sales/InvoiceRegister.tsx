3299e0793ca8ec9b4471c16265cd70b80934b0ac
SoumyaPShroff <bsoumyashri@gmail.com>
2026-02-27 17:44:27 +0530

    code updated

--- src/models/Sales/InvoiceRegister.tsx
+++ src/models/Sales/InvoiceRegister.tsx
@@ -27,18 +27,7 @@ const InvoiceRegister = () => {
   const handleExport = async () => {
     try {
       setLoading(true);
-
-      const response = await axios.post(
-        "/api/InvoiceRegister",
-        {
-          monthYear,
-          status,
-        },
-        {
-          responseType: "blob",
-        }
-      );
-
+      const response = await axios.post("/api/Sales/InvoiceRegister", { monthYear, status, },{responseType: "blob",});
       const url = window.URL.createObjectURL(new Blob([response.data]));
       const link = document.createElement("a");
       link.href = url;
