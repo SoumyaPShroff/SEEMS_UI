cc0890fdf002afa3de325ee9378e1954553601a2
SoumyaPShroff <bsoumyashri@gmail.com>
2026-07-24 09:36:38 +0530

    baseurl - removed comments

--- src/const/BaseUrl.ts
+++ src/const/BaseUrl.ts
@@ -45,7 +45,7 @@ const getDefaultApiBaseUrl = () => {
     return url;
   }
 
-  // Test server - not work,since local host and http, it considers above case only
+  // Test server 
   if (host === "10.60.2.141") {
     url = `http://${host}:7117`; // add port if needed
     console.log("API Base test URL:", url);
